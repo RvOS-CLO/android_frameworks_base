427d904d4b4425347a1f202e12c0d892ad78dc85
Rve27 <rve27github@gmail.com>
2024-11-21 08:59:46 +0800

    SystemUI: Change LAUNCHER_ACTIVITY_CLASS_NAME
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- packages/SystemUI/shared/src/com/android/systemui/shared/system/QuickStepContract.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/system/QuickStepContract.java
@@ -38,7 +38,7 @@ import java.util.StringJoiner;
 public class QuickStepContract {
     // Fully qualified name of the Launcher activity.
     public static final String LAUNCHER_ACTIVITY_CLASS_NAME =
-            "co.aospa.launcher.ParanoidLauncher";
+            "co.rvos.launcher.RvOSLauncher";
 
     public static final String KEY_EXTRA_SYSUI_PROXY = "extra_sysui_proxy";
     public static final String KEY_EXTRA_WINDOW_CORNER_RADIUS = "extra_window_corner_radius";
